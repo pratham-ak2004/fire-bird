38d77ea29f109d7eab2c52a0603a5a98b99ef89d
pratham-ak2004 <pratham20442@gmail.com>
2024-03-13 00:13:24 +0530

    feat: initial commit and home page

---
+++ src/app/_components/nav-bar.tsx
@@ -0,0 +1,55 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useTheme } from "next-themes";
+
+import { Label } from "~/components/ui/label";
+import { Button } from "~/components/ui/button";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { set } from "zod";
+
+export default function NavBar() {
+  const { setTheme , theme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <>
+      <div className="flex h-16 w-full justify-center bg-white dark:bg-black fixed bg-opacity-70 dark:bg-opacity-70">
+        <div className="flex h-full w-full max-w-screen-xl items-center p-4">
+          <Link className="flex w-fit flex-row items-center gap-4" href={"/"}>
+            <Image
+              src="/fire-bird-icon.png"
+              alt="Fire Bird"
+              width={1280}
+              height={720}
+              className="aspect-auto size-14"
+            ></Image>
+            <Label className="text-xl font-medium uppercase text-[#27426a] dark:text-[#5187dc]">
+              Fire Bird
+            </Label>
+          </Link>
+          <ul className="flex flex-row gap-4 ml-auto">
+            <li>
+              <Button variant="outline" size="icon" onClick={toggleTheme}>
+                  <SunIcon className="block dark:hidden" />
+                  <MoonIcon className="hidden dark:block" />
+              </Button>
+            </li>
+            <li>
+              <Button className="border-2 bg-white text-black hover:bg-slate-100 dark:bg-black dark:text-white hover:dark:bg-slate-800">
+                Sign In
+              </Button>
+            </li>
+            <li>
+              <Button className="hidden sm:block">Create Account</Button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
+
+import { CreatePost } from "~/app/_components/create-post";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  noStore();
+  const hello = await api.post.hello.query({ text: "from tRPC" });
+  const session = await getServerAuthSession();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+        </h1>
+       
+        <CrudShowcase />
+      </div>
+    </main>
+  );
+}
+
+async function CrudShowcase() {
+  const session = await getServerAuthSession();
+  if (!session?.user) return null;
+
+  const latestPost = await api.post.getLatest.query();
+
+  return (
+    <div className="w-full max-w-xs">
+      {latestPost ? (
+        <p className="truncate">Your most recent post: {latestPost.name}</p>
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+
+      <CreatePost />
+    </div>
+  );
+}
