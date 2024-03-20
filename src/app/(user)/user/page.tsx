4e591ed3c6d36a6731bc1d7c99e46dc7a8ef175e
pratham-ak2004 <pratham20442@gmail.com>
2024-03-21 00:40:21 +0530

    feat: mails UI layout

--- src/app/(user)/user/page.tsx
+++ src/app/(user)/user/page.tsx
@@ -1,40 +1,47 @@
-"use client"
-import React from 'react'
-import { SessionProvider } from 'next-auth/react'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import { useTheme } from 'next-themes'
+"use client";
+import React from "react";
+import { SessionProvider } from "next-auth/react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useTheme } from "next-themes";
 
-import { Label } from "~/components/ui/label"
-import { Skeleton } from '~/components/ui/skeleton'
+import { Label } from "~/components/ui/label";
+import { Skeleton } from "~/components/ui/skeleton";
 
-import "./styles.modules.css"
+import "./styles.modules.css";
 
 export default function User() {
   return (
     <SessionProvider>
       <Component />
     </SessionProvider>
-  )
+  );
 }
 
 function Component() {
   const session = useSession();
   const router = useRouter();
   const { theme } = useTheme();
-  
-  if(session.status === "authenticated"){
+
+  if (session.status === "authenticated") {
     setTimeout(() => {
-       router.push(`/user/${session.data.user.id}`)
-     },2000)
+      router.push(`/user/${session.data.user.id}`);
+    }, 2000);
   }
-  
-  return(
+
+  return (
     <>
-    <div className={`w-screen h-screen flex flex-col gap-5 justify-center items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] ${theme === "light" ? "bg-white" : "bg-black"}`}>
-        <Label className={`text-3xl ${theme === "light" ? "text-black" : "text-white"}`}> Loading your mails...</Label>
-        <Skeleton className={`w-10 h-2 bg-slate-500 custom-animation`} />
-    </div>
+      <div
+        className={`flex h-screen w-screen flex-col items-center justify-center gap-5 bg-gradient-to-b from-[#2e026d] to-[#15162c] ${theme === "light" ? "bg-white" : "bg-black"}`}
+      >
+        <Label
+          className={`text-3xl ${theme === "light" ? "text-black" : "text-white"}`}
+        >
+          {" "}
+          Loading your mails...
+        </Label>
+        <Skeleton className={`custom-animation h-2 w-10 bg-slate-500`} />
+      </div>
     </>
-  )
+  );
 }
--- src/app/_components/user-componects/sideBar.tsx
+++ src/app/_components/user-componects/sideBar.tsx
@@ -1,23 +1,35 @@
 "use client";
 import React from "react";
 import { useTheme } from "next-themes";
+import { usePathname, useSearchParams } from "next/navigation";
+import Link from "next/link";
 
-import { Button } from "~/components/ui/button";
 import { IoMdMail, IoMdChatbubbles } from "react-icons/io";
 import { MdVideoCall } from "react-icons/md";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { Button } from "~/components/ui/button";
 
 export default function SideBar() {
   const { theme, setTheme } = useTheme();
+  const params = useSearchParams();
+  const pathname = usePathname();
 
   return (
-    <div className="flex h-screen flex-col items-center justify-between py-4 px-2 bg-one">
-      <div className="flex flex-col gap-4">
-        <IoMdMail className="size-8 rounded-sm" />
-        <IoMdChatbubbles className="size-8" />
-        <MdVideoCall className="size-8" />
+    <div className="bg-one flex h-screen flex-col items-center justify-between px-2 py-4">
+      <div className="flex flex-col gap-4 items-center">
+        <Link className={`size-10  p-1 ${params.get("tab") === "mail" ? "bg-slate-700 rounded-md" : ""}`} href={{ pathname: pathname, query: { tab: "mail" } }}>
+          <IoMdMail className="size-8 rounded-sm" />
+        </Link>
+        <Link className={`size-10  p-1 ${params.get("tab") === "chat" ? "bg-slate-700 rounded-md" : ""}`} href={{ pathname: pathname, query: { tab: "chat" } }}>
+          <IoMdChatbubbles className="size-8" />
+        </Link>
+        <Link className={`size-10  p-1 ${params.get("tab") === "meet" ? "bg-slate-700 rounded-md" : ""}`} href={{ pathname: pathname, query: { tab: "meet" } }}>
+          <MdVideoCall className="size-8" />
+        </Link>
       </div>
       <Button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-        {theme}
+        <SunIcon className="block dark:hidden" />
+        <MoonIcon className="hidden dark:block" />
       </Button>
     </div>
   );
